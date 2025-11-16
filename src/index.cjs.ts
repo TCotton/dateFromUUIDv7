328b04f428bb9fe04fbaa61b3dc472efd0b83b07
TCotton <me@andywalpole.me>
2025-11-16 17:47:09 +0000

    chore: fixed typo

--- src/index.cjs.ts
+++ src/index.cjs.ts
@@ -10,7 +10,7 @@ import {
 /**
  * Extract date information from a UUIDv7.
  *
- * @deprecated CommonJS support is deprecated and will be removed in v3.0.0.
+ * @deprecated CommonJS support is deprecated and will be removed in future versions
  * Please migrate to ES modules: `import { dateFromUUIDv7 } from 'uuidv7-utilities'`
  * instead of `const { dateFromUUIDv7 } = require('uuidv7-utilities')`
  *
@@ -21,7 +21,7 @@ export const dateFromUUIDv7 = _dateFromUUIDv7;
 /**
  * Validate UUID version and return version identifier.
  *
- * @deprecated CommonJS support is deprecated and will be removed in v3.0.0.
+ * @deprecated CommonJS support is deprecated and will be removed in future versions
  * Please migrate to ES modules: `import { uuidVersionValidation } from 'uuidv7-utilities'`
  * instead of `const { uuidVersionValidation } = require('uuidv7-utilities')`
  *
@@ -32,7 +32,7 @@ export const uuidVersionValidation = _uuidVersionValidation;
 /**
  * Return type for dateFromUUIDv7 function.
  *
- * @deprecated CommonJS support is deprecated and will be removed in v3.0.0.
+ * @deprecated CommonJS support is deprecated and will be removed in future versions
  * Please migrate to ES modules: `import { DateFromUUIDv7Result } from 'uuidv7-utilities'`
  * instead of `const { DateFromUUIDv7Result } = require('uuidv7-utilities')`
  */
@@ -41,7 +41,7 @@ export type DateFromUUIDv7Result = _DateFromUUIDv7Result;
 /**
  * Union type representing possible UUID version identifiers.
  *
- * @deprecated CommonJS support is deprecated and will be removed in v3.0.0.
+ * @deprecated CommonJS support is deprecated and will be removed in future versions
  * Please migrate to ES modules: `import { UUIDVersionTuple } from 'uuidv7-utilities'`
  * instead of `const { UUIDVersionTuple } = require('uuidv7-utilities')`
  */
